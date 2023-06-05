cf56a406f9e7b1ba911be2962adeaae5d000a218
khanhlvm-window11 <khanhlvm98@gmail.com>
2023-06-05 13:02:54 +0700

    Added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 
 class HelloApiView(APIView):
@@ -34,22 +37,22 @@ class HelloApiView(APIView):
                 serializer.errors, 
                 status=status.HTTP_400_BAD_REQUEST
             )
-        
+
     def put(self, request, pk=None):
         """Handle updating an object"""
         return Response({'method': 'PUT'})
-    
+
     def patch(self, request, pk=None):
         """Handle a partial update of an object"""
         return Response({'method': 'PATCH'})
-    
+
     def delete(self, request, pk=None):
         """Delete an object"""
         return Response({'method': 'DELETE'})
 
+
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
-
     def list(self, request):
         """Return a hello message"""
         a_viewset = [
@@ -73,7 +76,7 @@ class HelloViewSet(viewsets.ViewSet):
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
             )
-        
+
     def retrieve(self, request, pk=None):
         """Handle getting an object by its ID"""
         return Response({'http_method': 'GET'})
@@ -89,4 +92,11 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
-    
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
